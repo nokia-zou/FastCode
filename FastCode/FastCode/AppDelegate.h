236e7eba966c65516956a93a9f859c995406682e
zone <nokia_zou@163.com>
2016-07-02 10:33:33 +0800

    add new pro

---
+++ FastCode/FastCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastCode
+//
+//  Created by 朋 邹 on 16/7/2.
+//  Copyright © 2016年 zou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
