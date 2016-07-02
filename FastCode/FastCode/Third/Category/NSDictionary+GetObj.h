3a1e13f405cae5fa23dcbb94ab497b0e1018bd2b
zone <nokia_zou@163.com>
2016-07-02 17:08:34 +0800

    dic get obj

--- FastCode/FastCode/Third/Category/NSDictionary+GetObj.h
+++ FastCode/FastCode/Third/Category/NSDictionary+GetObj.h
@@ -2,12 +2,88 @@
 //  NSDictionary+GetObj.h
 //  FastCode
 //
-//  Created by 朋 邹 on 16/7/2.
+//  Created by LP on 16/7/2.
 //  Copyright © 2016年 zou. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSDictionary (GetObj)
+
+/**
+ *  获取NSInteger值
+ *
+ *  @param key key
+ *
+ *  @return 默认返回：0
+ */
+- (NSInteger)integerValueForKey:(NSString *)key;
+
+- (NSInteger)integerValueForKey:(NSString *)key defaultValue:(NSInteger)value;
+
+/**
+ *  获取CGFloat值
+ *
+ *  @param key key
+ *
+ *  @return 默认返回：0
+ */
+- (CGFloat)floatValueForKey:(NSString *)key;
+
+/**
+ *  获取BOOL值
+ *
+ *  @param key key
+ *
+ *  @return 默认返回：NO
+ */
+- (BOOL)boolValueForKey:(NSString *)key;
+
+- (BOOL)boolValueForKey:(NSString *)key defaultValue:(BOOL)value;
+
+/**
+ *  获取NSString值
+ *
+ *  @param key key
+ *
+ *  @return 默认返回：nil
+ */
+- (NSString *)stringValueForKey:(NSString *)key;
+
+- (NSString *)stringValueForKey:(NSString *)key defaultValue:(NSString *)value;
+
+- (NSDictionary *)dictionaryValueForKey:(NSString *)key;
+
+- (NSArray *)arrayValueForKey:(NSString *)key;
+
+
+/**
+ *  获取NSNumber值
+ *
+ *  @param key key
+ *
+ *  @return 默认返回：nil
+ */
+- (NSNumber *)floatNumberValueForKey:(NSString *)key;
+
+/**
+ *  获取NSNumber值,值为NSInteger类型
+ *
+ *  @param key key
+ *
+ *  @return 默认返回：nil
+ */
+- (NSNumber *)integerNumberValueForKey:(NSString *)key;
+
+
+/**
+ *  获取 id 值
+ *
+ *  @param key       key
+ *  @param goalClass 期望返回数据的类型，不符合返回 nil
+ *
+ *  @return id
+ */
+- (id)valueForKey:(NSString *)key valueGoalClass:(Class)goalClass;
 
 @end
--- FastCode/FastCode/Third/Category/NSDictionary+GetObj.m
+++ FastCode/FastCode/Third/Category/NSDictionary+GetObj.m
@@ -2,12 +2,188 @@
 //  NSDictionary+GetObj.m
 //  FastCode
 //
-//  Created by 朋 邹 on 16/7/2.
+//  Created by LP on 16/7/2.
 //  Copyright © 2016年 zou. All rights reserved.
 //
 
 #import "NSDictionary+GetObj.h"
 
 @implementation NSDictionary (GetObj)
+
+- (NSInteger)integerValueForKey:(NSString *)key {
+    return [self integerValueForKey:key defaultValue:0];
+}
+
+- (NSInteger)integerValueForKey:(NSString *)key defaultValue:(NSInteger)value {
+    if (nil == key) return value;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //        LOG(@"value for key (%@) is nil",key);
+        return value;
+    }
+    
+    if ([result isKindOfClass:[NSString class]] || [result isKindOfClass:[NSNumber class]]) {
+        return [result integerValue];
+    } else {
+//        LOG(@"value type error for key (%@)",key);
+    }
+    
+    return value;
+}
+
+- (CGFloat)floatValueForKey:(NSString *)key {
+    if (nil == key) return 0;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //        LOG(@"value for key (%@) is nil",key);
+        return 0;
+    }
+    
+    if ([result isKindOfClass:[NSString class]] || [result isKindOfClass:[NSNumber class]]) {
+        return [result floatValue];
+    } else {
+//        LOG(@"value type error for key (%@)",key);
+    }
+    
+    return 0;
+}
+
+- (BOOL)boolValueForKey:(NSString *)key {
+    return [self boolValueForKey:key defaultValue:NO];
+}
+
+- (BOOL)boolValueForKey:(NSString *)key defaultValue:(BOOL)value {
+    if (nil == key) return value;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //        LOG(@"value for key (%@) is nil",key);
+        return value;
+    }
+    
+    if ([result isKindOfClass:[NSString class]] || [result isKindOfClass:[NSNumber class]]) {
+        return [result boolValue];
+    } else {
+//        LOG(@"value type error for key (%@)",key);
+    }
+    
+    return value;
+}
+
+- (NSString *)stringValueForKey:(NSString *)key {
+    return [self stringValueForKey:key defaultValue:nil];
+}
+
+- (NSString *)stringValueForKey:(NSString *)key defaultValue:(NSString *)value {
+    if (nil == key) return value;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //        LOG(@"string value for key (%@) is nil",key);
+        return value;
+    }
+    
+    if ([result isKindOfClass:[NSString class]]) {
+        return result;
+    }
+    
+    if ([result isKindOfClass:[NSNumber class]]) {
+        return [result stringValue];
+    }
+    
+    return value;
+}
+
+- (NSDictionary *)dictionaryValueForKey:(NSString *)key {
+    if (nil == key) return nil;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //        LOG(@"string value for key (%@) is nil",key);
+        return nil;
+    }
+    
+    if ([result isKindOfClass:[NSDictionary class]]) {
+        return result;
+    }
+    
+    return nil;
+}
+
+- (NSArray *)arrayValueForKey:(NSString *)key {
+    if (nil == key) return nil;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //        LOG(@"string value for key (%@) is nil",key);
+        return nil;
+    }
+    
+    if ([result isKindOfClass:[NSArray class]]) {
+        return result;
+    }
+    
+    return nil;
+}
+
+- (NSNumber *)floatNumberValueForKey:(NSString *)key {
+    if (nil == key) return nil;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //        LOG(@"string value for key (%@) is nil",key);
+        return nil;
+    }
+    
+    if ([result isKindOfClass:[NSNumber class]]) {
+        return result;
+    }
+    
+    if ([result isKindOfClass:[NSString class]]) {
+        return [NSNumber numberWithFloat:[result floatValue]];
+    }
+    
+    return nil;
+}
+
+- (NSNumber *)integerNumberValueForKey:(NSString *)key {
+    if (nil == key) return nil;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //                LOG(@"string value for key (%@) is nil",key);
+        return nil;
+    }
+    
+    if ([result isKindOfClass:[NSNumber class]]) {
+        return result;
+    }
+    
+    if ([result isKindOfClass:[NSString class]]) {
+        return [NSNumber numberWithInteger:[result integerValue]];
+    }
+    
+    return nil;
+}
+
+
+- (id)valueForKey:(NSString *)key valueGoalClass:(Class)goalClass {
+    if (nil == key) return nil;
+    
+    id result = [self objectForKey:key];
+    if (nil == result || [result isKindOfClass:[NSNull class]]) {
+        //        LOG(@"string value for key (%@) is nil",key);
+        return nil;
+    }
+    
+    if ([result isKindOfClass:goalClass]) {
+        return result;
+    }
+    
+    return nil;
+}
+
 
 @end
